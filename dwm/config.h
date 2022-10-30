185bf7ef5fc9eecd71609687aca29192b40acb43
Christopher Pane <chrispanecs@gmail.com>
2022-10-29 21:14:56 -0400

    regular backup

--- dwm/config.h
+++ dwm/config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -17,8 +17,10 @@ static const char col_cyan[]        = "#005577";
 #include "colors.h"
 static const char *colors[][3]      = {
 	/*               fg                 bg                 border   */
-	[SchemeNorm] = { COLOR15,    BACKGROUND, col_gray2 },
-	[SchemeSel]  = { BACKGROUND, COLOR3,    COLOR1 }, 
+	[SchemeNorm] = { COLOR15,    BACKGROUND, col_gray1 },
+	[SchemeSel]  = { BACKGROUND, "#d78787",    "#dea3e5" }, 
+	/* [SchemeNorm] = { COLOR15,    BACKGROUND, col_gray2 }, */
+	/* [SchemeSel]  = { BACKGROUND, COLOR3,    COLOR1 }, */
 	/* [SchemeNorm] = { col_gray3, col_gray1, col_gray2 }, */ 
 	/* [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  }, */
 };
